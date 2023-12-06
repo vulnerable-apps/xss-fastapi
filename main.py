6490c60d35a031db19066a517933d75f6b8c730b
alex-nightvision <alex@nightvision.net>
2023-12-06 16:24:27 -0500

    lint

--- main.py
+++ main.py
@@ -5,14 +5,18 @@ from starlette.responses import FileResponse
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
+
 @app.get("/")
 def main_page(request: Request):
-    query = request.query_params.get('query')
+    query = request.query_params.get("query")
     if not query:
         return templates.TemplateResponse("main_page.html", {"request": request})
     else:
         message = f"Sorry, no results were found for <b>{query}</b>. <a href='?'>Try again</a>."
-        return templates.TemplateResponse("results.html", {"request": request, "message": message})
+        return templates.TemplateResponse(
+            "results.html", {"request": request, "message": message}
+        )
+
 
 @app.get("/static/{file_path}")
 async def static_file(file_path: str):
